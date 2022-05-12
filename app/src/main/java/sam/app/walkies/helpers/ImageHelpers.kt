da9c0194863fc80b62f05c8d37ef80203a00a63c
samstudent2020 <72104257+samstudent2020@users.noreply.github.com>
2022-05-13 00:00:23 +0100

    remove comments

--- app/src/main/java/sam/app/walkies/helpers/ImageHelpers.kt
+++ app/src/main/java/sam/app/walkies/helpers/ImageHelpers.kt
@@ -13,7 +13,7 @@ fun showImagePicker(intentLauncher : ActivityResultLauncher<Intent>) {
     var chooseFile = Intent(Intent.ACTION_OPEN_DOCUMENT)
     chooseFile.type = "image/*"
     chooseFile = Intent.createChooser(chooseFile, R.string.select_walkiesLocation_image.toString())
-    //chooseFile.flags = (Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION or Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
+    chooseFile.flags = (Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION or Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
     intentLauncher.launch(chooseFile)
 }
 
